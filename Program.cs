a262d84aa0ca15e2eabaed745af5c167e0b8fff3
Cologne69 <kleanosiauw@student.ciputra.ac.id>
2021-12-10 11:20:10 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace W13_Latihan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input Kalimat : ");
+            string inputKalimat = Console.ReadLine();
+            while (inputKalimat.Length < 3)
+            {
+                Console.WriteLine("KATA PADA KALIMAT KURANG PANJANG");
+                Console.Write("Input Kalimat : ");
+                inputKalimat = Console.ReadLine();
+
+
+            }
+
+
+            Console.Write("Input Kata : ");
+            string inputKata = Console.ReadLine();
+
+            foreach (char Kata in inputKalimat)
+            {
+                Console.WriteLine("Statistic Huruf: \n");
+
+            }
+            
+            string test = inputKata.ToCharArray();
+            int brpKali = inputKalimat.Count(f => (f == inputKata));
+            Console.WriteLine(brpKali);
+        }
+    }
+}
